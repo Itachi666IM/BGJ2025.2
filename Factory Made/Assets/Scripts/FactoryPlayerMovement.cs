fd638f4d9feb6891adacdabddbac7573c70bd86d
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2025-08-25 15:36:02 +0530

    Added Rigidbody movement script and 3D drag and drpo

---
+++ Factory Made/Assets/Scripts/FactoryPlayerMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class FactoryPlayerMovement : MonoBehaviour
+{
+    private float yaw = 0f, pitch = 0f;
+    private Rigidbody rb;
+
+    public float walkSpeed = 4f;
+    public float sensitivity = 2f;
+
+    private void Awake()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+           
+    }
+
+    
+    void Update()
+    {
+        Look();   
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Look()
+    {
+        pitch -= Input.GetAxisRaw("Mouse Y") * sensitivity;
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
+        yaw += Input.GetAxisRaw("Mouse X") * sensitivity;
+        Camera.main.transform.localRotation = Quaternion.Euler(pitch, yaw, 0);
+    }
+
+    private void Movement()
+    {
+        Vector2 axis = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * walkSpeed;
+        Vector3 forward = new Vector3(-Camera.main.transform.right.z, 0f, Camera.main.transform.right.x);
+        Vector3 wishDirection = (forward * axis.x + Camera.main.transform.right * axis.y + Vector3.up * rb.linearVelocity.y);
+        rb.linearVelocity = wishDirection;
+    }
+}
