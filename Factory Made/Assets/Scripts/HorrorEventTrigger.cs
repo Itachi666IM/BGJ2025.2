450423aa49124abb92ed91445c61baece5bf229e
Sanj1t <rannenchi009sharingantoad2@gmail.com>
2025-08-25 20:11:43 +0530

    Created Menu Scene

---
+++ Factory Made/Assets/Scripts/HorrorEventTrigger.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HorrorEventTrigger : MonoBehaviour
+{
+    BoxCollider myCollider;
+    public UnityEvent triggerEvent;
+    bool once;
+    private void Awake()
+    {
+        myCollider = GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if(!once)
+            {
+                triggerEvent?.Invoke();
+            }
+        }
+    }
+}
---
+++ Factory Made/Assets/Scripts/SFX.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class SFX : MonoBehaviour
+{
+    AudioSource myAudio;
+    private void Awake()
+    {
+        myAudio = GetComponent<AudioSource>();
+        ManageSingleton();
+    }
+
+    void ManageSingleton()
+    {
+        int instance = FindObjectsByType<SFX>(FindObjectsSortMode.None).Length;
+        if(instance > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void PlayAnySFX(AudioClip audioClip)
+    {
+        myAudio.PlayOneShot(audioClip);
+    }
+}
