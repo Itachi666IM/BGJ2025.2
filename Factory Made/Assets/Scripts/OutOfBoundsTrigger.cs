a3d17f64ec0ae06daa4f4e5c2711636ade4d073f
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2025-08-26 20:53:06 +0530

    Added sack bounds

--- Factory Made/Assets/Scripts/OutOfBoundsTrigger.cs
+++ Factory Made/Assets/Scripts/OutOfBoundsTrigger.cs
@@ -1,9 +1,41 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.Collections;
 public class OutOfBoundsTrigger : MonoBehaviour
 {
-    private void OnTriggerExit(Collider other)
+
+    [SerializeField] private TMP_Text warningText;
+    public string[] warningSentences;
+    public float typeSpeed;
+    private void Awake()
     {
-        if(other.tag == "")
+        warningText.text = "";
+    }
+    IEnumerator Type(string sentence)
+    {
+        foreach(char c in sentence)
+        {
+            warningText.text += c;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "drag")
+        {
+            StartCoroutine(RestartScene());
+           
+        }
+    }
+
+    IEnumerator RestartScene()
+    {
+        string randomSentence = warningSentences[Random.Range(0, warningSentences.Length)];
+        StartCoroutine(Type(randomSentence));
+        yield return new WaitUntil(() => warningText.text == randomSentence);
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
