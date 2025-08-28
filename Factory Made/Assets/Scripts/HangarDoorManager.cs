b153ef908c33d71bc34a39e37733eca2cebd2f64
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2025-08-28 23:06:56 +0530

    Added Factory scene fixes and enhancements

--- Factory Made/Assets/Scripts/HangarDoorManager.cs
+++ Factory Made/Assets/Scripts/HangarDoorManager.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 
 public class HangarDoorManager : MonoBehaviour
 {
+    [SerializeField] private AudioClip doorCloseSound;
     [SerializeField] private GameObject hangarDoor;
+
+    private SFX sfxPlayer;
+    private void Start()
+    {
+        sfxPlayer = FindFirstObjectByType<SFX>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
             hangarDoor.SetActive(true);
+            sfxPlayer.PlayAnySFX(doorCloseSound);
             Destroy(gameObject);
         }
     }
--- Factory Made/Assets/Scripts/SackCollector.cs
+++ Factory Made/Assets/Scripts/SackCollector.cs
@@ -4,13 +4,16 @@ using UnityEngine.Events;
 public class SackCollector : MonoBehaviour
 {
     [SerializeField] private TMP_Text sackText;
+    [SerializeField] private AudioClip yeahSound;
+    [SerializeField] private GameObject smokeEffect;
     public UnityEvent InteriorEvent;
     private int totalSacks;
     private int currentSacks;
-
+    private SFX sfxPlayer;
     private void Awake()
     {
         totalSacks = FindObjectsByType<Sack>(FindObjectsSortMode.None).Length;
+        sfxPlayer = FindFirstObjectByType<SFX>();
         currentSacks = 0;
     }
     private void Start()
@@ -28,6 +31,8 @@ public class SackCollector : MonoBehaviour
     {
         if(other.tag == "drag")
         {
+            sfxPlayer.PlayAnySFX(yeahSound);
+            Instantiate(smokeEffect, transform.position, Quaternion.Euler(0, 90, 0));
             currentSacks++;
             Destroy(other);
             UpdateSackText();
